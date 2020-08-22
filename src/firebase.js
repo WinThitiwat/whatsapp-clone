b81675bf6065f6ce5febe2bade5383ec6ce8adae
WinThitiwat <win.thitiwat@gmail.com>
2020-08-22 16:37:25 -0700

    Initial commit, clean up all default react setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCOycBw-nyZgt0Ese9uho_-Zgk5-rDDY90",
+    authDomain: "whatsapp-clone-b6b1a.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-b6b1a.firebaseio.com",
+    projectId: "whatsapp-clone-b6b1a",
+    storageBucket: "whatsapp-clone-b6b1a.appspot.com",
+    messagingSenderId: "1007075267278",
+    appId: "1:1007075267278:web:02654c41d70830f4c0b69f"
+  };
