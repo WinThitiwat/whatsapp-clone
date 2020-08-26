da9d14e22bd99dc109744b459b5a9bda589b2796
WinThitiwat <win.thitiwat@gmail.com>
2020-08-25 23:10:29 -0700

    Create header, body, sidebars, and main chat body with their css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Let's build a WhatsApp Clone</h1>
+    //   BEM (Block Element Modifier) naming convention
+    <div className="app">
+        <div className="app_body">
+            {/* Sidebar */}
+            <Sidebar/>
+            {/* Chat */}
+            <Chat/>
+        </div>
     </div>
   );
 }
